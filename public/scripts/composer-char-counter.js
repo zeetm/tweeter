70b9a0ab68f3099b6f8c3003bdb1e6db1b57edc7
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2022-02-07 01:32:36 +0000

    implementing the chacter counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   // --- our code goes here ---
+  $('.textarea').on('output', function() {
+    let newTweetLength = $(this).val().length;
+    let nearbyCounter = $(this).siblings('.counter');
+    const tweetLengthLimit = 140;
+
+    if (newTweetLength > tweetLengthLimit) {
+      nearbyCounter.addClass('tweetTooLong');
+    } else if (newTweetLength <= tweetLengthLimit) {
+      nearbyCounter.removeClass('tweetTooLong');
+    }
+    nearbyCounter.text(tweetLengthLimit - newTweetLength);
+  });
 });
