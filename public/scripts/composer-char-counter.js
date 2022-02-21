036225677a3f9a14b7085cf0ecad5e1ff525d865
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2022-02-21 17:17:53 +0000

    refractor styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  maxCharacters = 140;
+  const $textArea = $('.new-tweet textarea');
 
-  $('#form__counter').text(maxCharacters);
-  
-  $('textarea').bind('keyup keydown', function() {
-      var count = $('#form__counter');
-      var characters = $(this).val().length;
-  
-      if (characters > maxCharacters) {
-          count.addClass('over');
-      } else {
-          count.removeClass('over');
-      }
-  
-      count.text(maxCharacters - characters);
+  // updates the character counter every time text is inputted
+  $textArea.on('keyup', function() {
+
+    const $charCounter = $(this).siblings('.counter');
+    const remainingCharCount = 140 - $(this).val().length;
+
+    $charCounter.text(remainingCharCount);
+
+    // makes character counter red if over character limit
+    if (remainingCharCount < 0) {
+      $charCounter.addClass('over-char-limit');
+
+    } else {
+      $charCounter.removeClass('over-char-limit');
+    }
   });
 });
