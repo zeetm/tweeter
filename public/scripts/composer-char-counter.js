b3c13f1ec36e08bd4adac895531c19cda1bba1b4
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2022-02-21 15:18:33 +0000

    refreactor code for counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $('.textarea').on('output', function() {
-    let newTweetLength = $(this).val().length;
-    let nearbyCounter = $(this).siblings('.counter');
-    const tweetLengthLimit = 140;
+  maxCharacters = 140;
 
-    if (newTweetLength > tweetLengthLimit) {
-      nearbyCounter.addClass('tweetTooLong');
-    } else if (newTweetLength <= tweetLengthLimit) {
-      nearbyCounter.removeClass('tweetTooLong');
-    }
-    nearbyCounter.text(tweetLengthLimit - newTweetLength);
+  $('#form__counter').text(maxCharacters);
+  
+  $('textarea').bind('keyup keydown', function() {
+      var count = $('#form__counter');
+      var characters = $(this).val().length;
+  
+      if (characters > maxCharacters) {
+          count.addClass('over');
+      } else {
+          count.removeClass('over');
+      }
+  
+      count.text(maxCharacters - characters);
   });
 });
