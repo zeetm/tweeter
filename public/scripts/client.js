b094e3bb8e14817b4238394c51f55ab00e44e975
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2022-02-23 20:51:07 +0000

    remove hard-codes tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,15 +5,7 @@
  */
 // Test / driver code (temporary). Eventually will get this from the server.
 const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
+
 }
 
 const $tweet = function createTweetElement(tweetData) {
